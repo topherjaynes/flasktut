5237f501b3ace9460390d76b2d28333cc4ef1f8d
topherjaynes <chris.jaynes@gmail.com>
2013-03-26 22:15:43 -0400

    change db model, update requirements.txt

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = 'postgresql://topherjaynes:Clemson1!@localhost/flasktut'
+
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 CSRF_ENABLED = True
